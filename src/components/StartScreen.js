b11810049d71e04daeabe1bacfd4a7937776162c
Ehsan <xdehsanxd@gmail.com>
2024-08-05 19:49:19 +0330

    main

---
+++ src/components/StartScreen.js
@@ -0,0 +1,17 @@
+function StartScreen({ numQuestions, dispatch }) {
+  function handleStartQuiz() {
+    dispatch({ type: "start" });
+  }
+
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button className="btn btn-ui" onClick={handleStartQuiz}>
+        Let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
